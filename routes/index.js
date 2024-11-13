cbd00682c6a0221876b8069d1e8181a0da40a1db
Kimhyunmook <gusanr4200@naver.com>
2024-11-13 11:12:59 +0900

    스터디 상세

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 import express from "express";
 import studyDetail from "./studyDetail/router.js";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const router = express.Router();
 router.use("/studydetail", studyDetail);
+
+//home 용 임시
+router.get("/home", async (req, res) => {
+  let status = 0;
+  let result = {};
+  try {
+    status = 201;
+  } catch (err) {
+    console.error(err);
+    status = 400;
+  }
+  res.status(status).send(result);
+});
 
 export default router;
--- routes/studyDetail/router.js
+++ routes/studyDetail/router.js
@@ -5,23 +5,26 @@ const prisma = new PrismaClient();
 
 //조회
 
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
   let result = {
-    msg: "",
+    msg: "조회 성공",
   };
   try {
-    const data = await prisma.study.findMany({
+    const data = await prisma.study.findUnique({
       where: {
         id,
       },
+      include: {
+        emojis: true,
+        habit: true,
+      },
     });
     result.data = data;
+    res.status(201).send(result);
   } catch (error) {
     console.error("errorMsg : ", error);
-    result.msg = error;
   }
-  res.status(201).send(result);
 });
 // router.get("/", service.getMain);
 // router.get("/about", service.getMianAbout);
