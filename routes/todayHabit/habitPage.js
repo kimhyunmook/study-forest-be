e2b32758571591f1ce0243e80cbedd57edcfc7d1
tori_0510 <luvin6626@naver.com>
2024-11-24 00:36:38 +0900

    오늘의습관Schema.Prisma 제작

---
+++ routes/todayHabit/habitPage.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+
+
+
+
+
+
+export default router;
