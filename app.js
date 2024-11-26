825014316b25ee343423eff0d004897a7cdcac34
soohwanpak <qoiuytnghks4@gmail.com>
2024-11-27 05:28:37 +0900

    습관쪽 수정

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["*"],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT", "OPTIONS"],
 
